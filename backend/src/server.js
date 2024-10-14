aae704c377c3c0b658a7a4af0b848a5df06dbded
Namish Prasad <namishprasad180@gmail.com>
2024-10-14 22:03:56 +0530

    bugs cleared

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path';
 import { router as authRoute } from "./routes/auth.route.js";
 import { router as problemRoute } from "./routes/problem.route.js";
 import { router as codeRoutes } from "./routes/submitCode.route.js";
+
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +25,13 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/problems", problemRoute);
 app.use("/api/problem", codeRoutes);
+
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 const PORT = process.env.PORT;
 
